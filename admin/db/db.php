5c4209c782594e359bb69a55526aadb7ff0c6a74
Lisa Jones <paige4521@users.noreply.github.com>
2021-09-11 19:45:31 +0600

    Update db.php

--- admin/db/db.php
+++ admin/db/db.php
@@ -1,7 +1,8 @@
 <?php
 	require_once 'config.php';
 	try {
-		$dsn = "pgsql:host=$host;port=5432;dbname=$db;";
+		//$dsn = "pgsql:host=$host;port=5432;dbname=$db;";
+		$dsn = "pgsql:host=$host;dbname=$db;";
 		// make a database connection
 		$pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);	
 
