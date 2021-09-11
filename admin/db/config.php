bf8f966905a9d2303ff3989f2f76df9baf34fcd0
Lisa Jones <paige4521@users.noreply.github.com>
2021-09-11 19:44:34 +0600

    Update config.php

--- admin/db/config.php
+++ admin/db/config.php
@@ -1,5 +1,5 @@
 <?php
-$host= 'ec2-18-215-44-132.compute-1.amazonaws.com';
+$host= 'ec2-18-215-44-132.compute-1.amazonaws.com:5432';
 $db = 'de3tpd5vt7fb0b';
 $user = 'llfkqcwukflktc';
 $password = '35af897fd853f3006d773a02b34474c956a0ad96767870d8fbe6e1b59ea9afad';
