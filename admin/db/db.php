2fa0bc3cb17fc24b886341b842e9e8f11ec70e6f
Lisa Jones <paige4521@users.noreply.github.com>
2021-09-11 21:49:19 +0600

    Update db.php

--- admin/db/db.php
+++ admin/db/db.php
@@ -6,12 +6,11 @@
 
 		$pdo = new PDO("pgsql:" . sprintf(
 		    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-		    $db["ec2-18-215-44-132.compute-1.amazonaws.com"],
-		    $db["5432"],
-		    $db["llfkqcwukflktc"],
-		    $db["35af897fd853f3006d773a02b34474c956a0ad96767870d8fbe6e1b59ea9afad
-URI"],
-		    ltrim($db["de3tpd5vt7fb0b"], "/")
+		    $db["host"],
+		    $db["port"],
+		    $db["user"],
+		    $db["pass"],
+		    ltrim($db["path"], "/")
 		));
 		
 		
